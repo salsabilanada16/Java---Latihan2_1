95c9903ac2e792a13ef240ef4442d570a83dc863
salsabilanada16 <sadzani16@gmail.com>
2020-02-21 21:35:30 +0700

    Program01 java latihan 2-1

---
+++ Program01.java
@@ -0,0 +1,8 @@
+public class Program01
+{
+	public void run()
+	{
+		System.out.printf("Saya mahasiswa %d IF Universitas Pasundan. \n", 2019);
+		System.out.println("Saya akan belajar pemrograman dengan sungguh-sungguh.");
+	}
+}
